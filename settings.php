8a7fb62e17e091232148fdea5e01a84586de31fb
Nicosoft <nicolas.jourdain@gmail.com>
2018-10-08 14:21:14 +1100

    First commit

---
+++ settings.php
@@ -0,0 +1,35 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Settings for the User Grade Report
+ *
+ * @package     report_ncmusergrades
+ * @copyright   2018 Nicolas Jourdain <nicolas.jourdain@navitas.com>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+$ADMIN->add(
+    'reports', 
+    new admin_externalpage(
+        'reportlinkname',
+        get_string('reportlinkname', 'report_ncmusergrades'),
+        "$CFG->wwwroot/report/ncmusergrades/index.php",'moodle/backup:backupcourse'));
+
+// no report settings
+$settings = null;
