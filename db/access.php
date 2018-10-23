20fef67b296fa9c83bcb1c609fe247eba58c8252
Nicosoft <nicolas.jourdain@gmail.com>
2018-10-23 17:00:55 +1100

    Change capabilities

--- db/access.php
+++ db/access.php
@@ -1,5 +1,5 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
+// This file is part of the Zoom plugin for Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -15,17 +15,21 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Plugin version and other meta-data are defined here.
  *
  * @package     report_ncmusergrades
+ * @category    upgrade
  * @copyright   2018 Nicolas Jourdain <nicolas.jourdain@navitas.com>
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
-defined('MOODLE_INTERNAL') || die();
-
-$plugin->component = 'report_ncmusergrades';
-$plugin->release = '0.1.0';
-$plugin->version = 2018102303;
-$plugin->requires = 2017111300;
-$plugin->maturity = MATURITY_ALPHA;
+defined ( 'MOODLE_INTERNAL' ) || die ();
+$capabilities = array(
+  'report/ncmusergrades:use' => array(
+    'riskbitmask' => RISK_PERSONAL,
+    'captype' => 'read',
+    'contextlevel' => CONTEXT_SYSTEM,
+    'archetypes' => array (
+      'editingteacher' => CAP_ALLOW,
+      'manager' => CAP_ALLOW
+    )
+  ),
+);
