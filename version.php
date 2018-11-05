7154ee35e0b0c6916117af18447a29f9ab099461
Nicosoft <nicolas.jourdain@gmail.com>
2018-11-06 10:45:18 +1100

    Bump version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'report_ncmusergrades';
-$plugin->release = '1.0.1';
-$plugin->version = 2018110601;
+$plugin->release = '1.0.2';
+$plugin->version = 2018110603;
 $plugin->requires = 2017111300;
 $plugin->maturity = MATURITY_STABLE;
